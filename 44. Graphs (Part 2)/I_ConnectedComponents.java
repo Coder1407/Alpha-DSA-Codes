5646388c2c117964c5746e1ecc4585cb35102321
Coder1407 <amitesh1407@gmail.com>
2024-01-28 22:23:41 +0530

    Graph directory created

---
+++ 44. Graphs (Part 2)/I_ConnectedComponents.java
@@ -0,0 +1,5 @@
+public class I_ConnectedComponents {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Graphs/I_Representation.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class I_Representation {
+    public static void main(String[] args) {
+
+        /*
+                    1---2
+                     \ /
+                      3
+         */
+        int n=3, m=3;  // n-> nodes,  m-> edges
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
+        
+        //n+1
+        for(int i=0; i<=n; i++) {
+            adj.add(new ArrayList<Integer>());
+        }
+
+        //edge 1->2
+        adj.get(1).add(2);
+        adj.get(2).add(1);
+
+        //edge 2->3
+        adj.get(2).add(3);
+        adj.get(3).add(2);
+
+        //edge 1->3
+        adj.get(1).add(3);
+        adj.get(3).add(1);
+
+        //print all edges
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<adj.get(i).size(); j++) {
+                System.out.print(adj.get(i).get(i));
+            }
+        }
+        System.out.println();
+    }
+}
